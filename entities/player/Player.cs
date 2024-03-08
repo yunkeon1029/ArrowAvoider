d98b212d69896c9e2f9d3c918ff006e4ce5faf94
yunkeon1029 <yunkeon1029@gmail.com>
2024-03-08 20:12:28 +0900

    added invincibility time to player

--- entities/player/Player.cs
+++ entities/player/Player.cs
@@ -1,14 +1,22 @@
+using System;
 using Godot;
 
 public partial class Player : CharacterBody2D
 {
     [Export]
     public HealthManager HealthManager { get; private set; }
+    [Export]
+    public HitReceiver HitReceiver { get; private set; }
 
     [Export] 
     private float _maxSpeed;
     [Export] 
     private float _accelerationRate;
+
+    [Export]
+    private float _invincibilityTime;
+    [Export]
+    private Color _invincibillityMoudulate;
 
     private Vector2 _velocity;
 
@@ -34,6 +42,11 @@ public partial class Player : CharacterBody2D
         return moveInput;
 	}
 
+    public override void _Ready()
+    {
+        HitReceiver.Damaged += _ => StartInvincibillity();
+    }
+
     public override void _PhysicsProcess(double elapsedTime)
     {
 		Vector2 moveInput = GetMoveInput();
@@ -45,5 +58,22 @@ public partial class Player : CharacterBody2D
 
         if (collisionResult != null)
             _velocity = Vector2.Zero;
+    }
+
+    private void StartInvincibillity()
+    {
+        SceneTree tree = GetTree();
+        SceneTreeTimer invincibillityTimer = tree.CreateTimer(_invincibilityTime);
+
+        Color previousMoudulate = Modulate;
+        uint previousHitLayer = HitReceiver.CollisionLayer;
+
+        invincibillityTimer.Timeout += () => Modulate = previousMoudulate;
+        invincibillityTimer.Timeout += () => HitReceiver.ModifyDamage -= InvincibillityMod;
+
+        HitReceiver.ModifyDamage += InvincibillityMod;
+        Modulate = _invincibillityMoudulate;
+
+        static void InvincibillityMod(ref float damage) => damage = 0;
     }
 }
