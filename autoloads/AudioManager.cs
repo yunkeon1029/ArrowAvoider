51b4948274fb4b912395cc04de791cdd9cf0e57d
yunkeon1029 <yunkeon1029@gmail.com>
2024-04-04 21:28:28 +0900

    Added AudioManager
    - added audio manager
    - added GemCollectedSound

---
+++ autoloads/AudioManager.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+
+public partial class AudioManager : Node, ISingleton
+{
+    public AudioManager()
+    {
+        TreeEntered += () => Singletons.AddInstance(this);
+        TreeExited += () => Singletons.RemoveInstance(this);
+    }
+
+    public void PlayAudio(AudioStream audioStream, Action<AudioStreamPlayer> modifySound = null)
+    {
+        AudioStreamPlayer instance = new();
+        AddChild(instance);
+
+        instance.Stream = audioStream;
+        instance.Play();
+
+        modifySound?.Invoke(instance);
+        instance.Finished += () => instance.QueueFree();
+    }
+}
--- prefabs/gem/Gem.cs
+++ prefabs/gem/Gem.cs
@@ -6,9 +6,18 @@ internal partial class Gem : AnimatableBody2D, IDespawnable
 	[Export]
 	private Area2D _hitArea;
 	[Export]
+	private AudioStream _collectedSound;
+
+	[Export]
+	private float _minCollectedSoundPitch;
+	[Export]
+	private float _maxCollectedSoundPitch;
+
+	[Export]
 	private int _score;
 
 	private ScoreManager _scoreManager;
+	private AudioManager _audioManager;
 
     public override void _Ready()
     {
@@ -17,6 +26,7 @@ internal partial class Gem : AnimatableBody2D, IDespawnable
 		_hitArea.AreaEntered += OnHit;
 		_hitArea.BodyEntered += OnHit;
 
+		_audioManager = Singletons.GetInstance<AudioManager>();
 		_scoreManager = game.ScoreManager;
 
 		RequestReady();
@@ -28,6 +38,16 @@ internal partial class Gem : AnimatableBody2D, IDespawnable
 			return;
 
 		_scoreManager.Score += _score;
+		_audioManager.PlayAudio(_collectedSound, RandomizePitch);
+
 		QueueFree();
+	}
+
+	private void RandomizePitch(AudioStreamPlayer audioPlayer)
+	{
+		float minPitch = _minCollectedSoundPitch;
+		float maxPitch = _maxCollectedSoundPitch;
+
+		audioPlayer.PitchScale = (float)GD.RandRange(minPitch, maxPitch);
 	}
 }
