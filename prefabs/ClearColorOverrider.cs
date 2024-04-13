1cce3cea388ad44503585886ac6c9b4f0dbf1051
yunkeon1029 <yunkeon1029@gmail.com>
2024-04-13 19:48:02 +0900

    Modified ClearColorOverrider

--- prefabs/ClearColorOverrider.cs
+++ prefabs/ClearColorOverrider.cs
@@ -1,16 +1,34 @@
 using Godot;
 using System;
 
+[Tool]
 internal partial class ClearColorOverrider : Node
 {
 	[Export]
+	public Color ClearColor
+	{
+		get => _clearColor;
+		set => SetClearColor(value);
+	}
+
 	private Color _clearColor;
 
 	public ClearColorOverrider()
 	{
+		if (Engine.IsEditorHint())
+			return;
+
 		Color defaultColor = RenderingServer.GetDefaultClearColor();
 
 		TreeEntered += () => RenderingServer.SetDefaultClearColor(_clearColor);
 		TreeExited += () => RenderingServer.SetDefaultClearColor(defaultColor);
+	}
+
+    public void SetClearColor(Color value)
+	{
+		if (!Engine.IsEditorHint())
+			RenderingServer.SetDefaultClearColor(value);
+
+		_clearColor = value;
 	}
 }
