43b7e5ebf212585d6803d1bad39262fd39043774
yunkeon1029 <124570743+yunkeon1029@users.noreply.github.com>
2024-04-21 23:09:28 +0900

    Fixed a bug

--- autoloads/AudioManager.cs
+++ autoloads/AudioManager.cs
@@ -16,6 +16,7 @@ internal partial class AudioManager : Node, ISingleton
         int bgmBusIndex = AudioServer.GetBusIndex("BGM");
         int sfxBusIndex = AudioServer.GetBusIndex("SFX");
 
+
         float bgmVolume = (float?)saveManager.GetData("BgmVolume") ?? 0.5f;
         float sfxVolume = (float?)saveManager.GetData("SfxVolume") ?? 0.5f;
 
